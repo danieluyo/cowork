7862f70db0d79e92d32c9e940cd49127df5d99c8
Mohamed Kawsara <mkwsra@gmail.com>
2015-12-24 02:25:37 +0200

    :construction: Minor tweak in 503 error view.

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -5,10 +5,10 @@
 @section('content')
     <header>
         <h1 class="animation-slide-top">503</h1>
-        <p>Be Right Back!</p>
+        <p><i class=" text-warning icon md-settings icon-spin page-maintenance-icon" aria-hidden="true"></i> Be Right Back!</p>
     </header>
-    <p class="error-advise">Hello there! our lovely website is in maintenance mode <br>
-        check back very very soon.
+    <p class="error-advise">
+        PLEASE GIVE US A MOMENT TO SORT THINGS OUT.
     </p>
     {{--<a class="btn btn-primary btn-round" href="../index.html">Che</a>--}}
 @endsection
