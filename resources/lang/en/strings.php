d7e4bad74f09be4e5f414c672e3a66a6e38e2fb5
Mohamed Kawsara <mkwsra@gmail.com>
2015-12-24 02:08:15 +0200

    Add strings file with App name and description

---
+++ resources/lang/en/strings.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used all the time for various
+    | messages that we need to display to the user.
+    |
+    */
+
+    'app_name' => 'Co.Work',
+    'app_desc' => 'Co.Work Project Description goes here',
+
+];
