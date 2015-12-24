2ae441c7ab97c6d56acd3caf28f4f26f383f18af
Mohamed Kawsara <mkwsra@gmail.com>
2015-12-24 03:58:31 +0200

    :telescope: new section body-class in errors view & view minor changes

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,11 +1,12 @@
-@extends('errors.master')
+@extends('layouts.errors')
 
 @section('title','Be Right Back')
+@section('body-class','page-error-404')
 
 @section('content')
     <header>
         <h1 class="animation-slide-top">503</h1>
-        <p><i class=" text-warning icon md-settings icon-spin page-maintenance-icon" aria-hidden="true"></i> Be Right Back!</p>
+        <p><i class="text-warning icon md-settings icon-spin page-maintenance-icon" aria-hidden="true"></i> Be Right Back!</p>
     </header>
     <p class="error-advise">
         PLEASE GIVE US A MOMENT TO SORT THINGS OUT.
