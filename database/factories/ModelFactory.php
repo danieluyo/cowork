dfe303b518bd54fdd70f919e4e4270ce276924a8
Mohamed Kawsara <mkwsra@gmail.com>
2015-12-25 04:25:03 +0200

    Extend user factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,11 +11,32 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
-    return [
-        'name' => $faker->name,
-        'email' => $faker->email,
-        'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
+$factory->define( App\Models\User::class, function( Faker\Generator $faker ) {
+	return [
+		'remember_token' => str_random( 10 ),
+		'media_id'       => $faker->imageUrl( 128, 128 ),
+		'city_id'        => 564123,
+        'region_id'      => 23334,
+        'country_id'     => 233,
+        'first_name'     => $faker->firstName,
+        'last_name'      => $faker->lastName,
+        'about_me'       => $faker->paragraph,
+        'birth_year'     => $faker->year,
+        'username'       => $faker->userName,
+        'email'          => $faker->email,
+        'password'       => bcrypt( "123456" ),
+        'role'           => "USER",
+        'address'        => $faker->address,
+        'zip'            => $faker->randomDigit,
+        'latitude'       => $faker->latitude,
+        'longitude'      => $faker->longitude,
+        'phone'          => $faker->phoneNumber,
+        'website'        => $faker->url,
+        'facebook'       => $faker->url,
+        'settings'       => "{}",
+        'payment'        => "{}",
+        'followers'      => $faker->randomDigit,
+        'followings'     => $faker->randomDigit,
+        'accessed_at'    => $faker->date(),
     ];
-});
+} );
