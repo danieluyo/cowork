48ab51cf1cb78d1b3182f02a00203bc06cb02762
Mohamed Kawsara <mkwsra@gmail.com>
2015-12-26 20:30:40 +0200

    Users Listings & Media factories

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -14,29 +14,60 @@
 $factory->define( App\Models\User::class, function( Faker\Generator $faker ) {
 	return [
 		'remember_token' => str_random( 10 ),
-		'media_id'       => $faker->imageUrl( 128, 128 ),
+		'media_id'       => rand( 1, 10 ),
 		'city_id'        => 564123,
-        'region_id'      => 23334,
-        'country_id'     => 233,
-        'first_name'     => $faker->firstName,
-        'last_name'      => $faker->lastName,
-        'about_me'       => $faker->paragraph,
-        'birth_year'     => $faker->year,
-        'username'       => $faker->userName,
-        'email'          => $faker->email,
-        'password'       => bcrypt( "123456" ),
-        'role'           => "USER",
-        'address'        => $faker->address,
-        'zip'            => $faker->randomDigit,
-        'latitude'       => $faker->latitude,
-        'longitude'      => $faker->longitude,
-        'phone'          => $faker->phoneNumber,
-        'website'        => $faker->url,
-        'facebook'       => $faker->url,
-        'settings'       => "{}",
-        'payment'        => "{}",
-        'followers'      => $faker->randomDigit,
-        'followings'     => $faker->randomDigit,
-        'accessed_at'    => $faker->date(),
-    ];
+		'region_id'      => 23334,
+		'country_id'     => 233,
+		'first_name'     => $faker->firstName,
+		'last_name'      => $faker->lastName,
+		'about_me'       => $faker->paragraph,
+		'birth_year'     => $faker->year,
+		'username'       => $faker->userName,
+		'email'          => $faker->email,
+		'password'       => bcrypt( "123456" ),
+		'role'           => "USER",
+		'address'        => $faker->address,
+		'zip'            => $faker->randomDigit,
+		'latitude'       => $faker->latitude,
+		'longitude'      => $faker->longitude,
+		'phone'          => $faker->phoneNumber,
+		'website'        => $faker->url,
+		'facebook'       => $faker->url,
+		'settings'       => "{}",
+		'payment'        => json_encode( $faker->creditCardDetails ),
+		'followers'      => $faker->randomDigit,
+		'followings'     => $faker->randomDigit,
+		'accessed_at'    => $faker->date(),
+	];
+} );
+
+
+$factory->define( App\Models\Listing::class, function( Faker\Generator $faker ) {
+	return [
+		'user_id'              => rand( 1, 10 ),
+		'city_id'              => 564123,
+		'region_id'            => 23334,
+		'country_id'           => 233,
+		'category_id'          => rand( 1, 3 ),
+		'type'                 => rand( 1, 2 ),
+		'hourly_price'         => $faker->randomDigit( 30, 10 ),
+		'hourly_price'         => $faker->randomDigit( 100, 50 ),
+		'monthly_price'        => $faker->randomDigit( 500, 200 ),
+		'max_number_of_people' => rand( 1, 10 ),
+		'area'                 => rand( 20, 150 ),
+		'ratings'              => $faker->randomFloat( 30, 10 ),
+		'impacts'              => rand( 0, 200 ),
+		'address'              => $faker->address,
+		'zip'                  => $faker->randomDigit,
+		'latitude'             => $faker->latitude,
+		'longitude'            => $faker->longitude,
+	];
+} );
+
+
+$factory->define( \App\Models\Media::class, function( Faker\Generator $faker ) {
+	return [
+		'original_name' => $faker->userName . $faker->fileExtension,
+		'filename'      => $faker->imageUrl(),
+	];
 } );
