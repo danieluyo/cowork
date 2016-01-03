0bebca9776e5456aa8ce7ec2f955dd0fbf488995
Mohamed Kawsara <mkwsra@gmail.com>
2016-01-03 12:51:27 +0200

    Rearranging and removing extra routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,36 +1,5 @@
 <?php
 
-
-/*
-|--------------------------------------------------------------------------
-| Front-end Routes
-|--------------------------------------------------------------------------
-*/
-Route::group(
-	[
-//		'prefix'     => Localization::setLocale(),
-		'middleware' => [
-//			'localization-session-redirect',
-//			'localization-redirect',
-			'web',
-		],
-		'as' => 'frontend.',
-	],
-	function() {
-		Route::get( '/', [ 'uses' => 'FrontendController@home', 'as' => 'home' ] );
-		Route::get( 'home', [ 'uses' => 'FrontendController@home', 'as' => 'home' ] );
-		Route::get( 'about-us', [ 'uses' => 'FrontendController@aboutUs', 'as' => 'aboutUs' ] );
-		Route::get( 'contact-us', [ 'uses' => 'FrontendController@contactUs', 'as' => 'contactUs' ] );
-		Route::post( 'contact-us', [ 'uses' => 'FrontendController@postContact', 'as' => 'contactUs.post' ] );
-
-		Route::get( 'login', [ 'uses' => 'Auth\AuthController@getLogin', 'as' => 'login' ] );
-		Route::post( 'login', [ 'uses' => 'Auth\AuthController@postLogin', 'as' => 'postLogin' ] );
-		Route::get( 'logout', [ 'uses' => 'Auth\AuthController@getLogout', 'as' => 'logout' ] );
-		Route::get( 'register', [ 'uses' => 'Auth\AuthController@getRegister', 'as' => 'register' ] );
-		Route::post( 'register', [ 'uses' => 'Auth\AuthController@postRegister', 'as' => 'postRegister' ] );
-	}
-);
-
 /*
 |--------------------------------------------------------------------------
 | Back-end Routes
@@ -48,10 +17,20 @@ Route::group(
 	],
 	function() {
 		Route::get( '/', [ 'uses' => 'DashboardController@home', 'as' => 'dashboard.index' ] );
-		Route::get( '/users', [ 'uses' => 'UserController@all', 'as' => 'dashboard.users' ] );
+		Route::get( '/users', [ 'uses' => 'UserController@index', 'as' => 'dashboard.users' ] );
 
+		Route::resource('categories','CategoryController');
+		Route::resource('listings','Listing\ListingController');
 	}
 );
+
+
+/*
+|--------------------------------------------------------------------------
+| Miscellaneous Routes
+|--------------------------------------------------------------------------
+*/
+Route::get( 'logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index' );
 
 
 /*
@@ -65,15 +44,48 @@ Route::group(
 		'as'     => 'ajax.',
 	],
 	function() {
-		Route::get( '/api/{someId}', 'AjaxController@getDownloads', [ 'as' => 'get-downloads' ] )->where( [ 'someId' => '[0-9]+' ] );
+
 	}
 );
 
-// Todo Add # symbol to users show route, to gain more SEO traffic
 
 /*
 |--------------------------------------------------------------------------
-| Miscellaneous Routes
+| Front-end Routes
 |--------------------------------------------------------------------------
 */
-Route::get( 'logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index' );
+Route::group(
+	[
+//		'prefix'     => Localization::setLocale(),
+		'middleware' => [
+//			'localization-session-redirect',
+//			'localization-redirect',
+			'web',
+		],
+		'as'         => 'frontend.',
+	],
+	function() {
+		Route::get( '/', [ 'uses' => 'FrontendController@home', 'as' => 'home' ] );
+		Route::get( 'home', [ 'uses' => 'FrontendController@home', 'as' => 'home' ] );
+		Route::get( 'about-us', [ 'uses' => 'FrontendController@aboutUs', 'as' => 'aboutUs' ] );
+		Route::get( 'contact-us', [ 'uses' => 'FrontendController@contactUs', 'as' => 'contactUs' ] );
+		Route::post( 'contact-us', [ 'uses' => 'FrontendController@postContact', 'as' => 'contactUs.post' ] );
+
+
+		Route::get( '/coffices/{user}/category/{category}/{listing}', [ 'uses' => 'Listing\UserListingController@show' ] );
+		Route::get( '/coffices/{user}/category/{category}/', [ 'uses' => 'Listing\UserListingController@index' ] );
+		Route::get( '/coffices/{country}/category/{category}/', [ 'uses' => 'Listing\CountryCategoryListingController@index' ] );
+		Route::get( '/coffices/{country}/category/{category}/{listing}', [ 'uses' => 'Listing\CountryCategoryListingController@show' ] );
+
+//		Route::get( '/coffices/category/{category}/', [ 'uses' => 'Listing\CategoryListingController@index' ] );
+//		Route::get( '/coffices/category/{category}/{listing}', [ 'uses' => 'Listing\CategoryListingController@show' ] );
+
+		Route::auth();
+
+		Route::get( '/{user}', [ 'uses' => 'FrontendController@profile' ] );
+	}
+);
+
+
+
+
