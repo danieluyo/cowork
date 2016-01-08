73b1be5e741ba3f52e35cd4c867611db97a7052f
Mohamed Kawsara <mkwsra@gmail.com>
2016-01-08 09:22:20 +0200

    Add error partial view

--- resources/views/errors/alert.blade.php
+++ resources/views/errors/alert.blade.php
@@ -1,4 +1,13 @@
-<div class="alert alert-{{ $type  }} alert-page alert-dark">
-    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-    {!! $message  !!}
+<div class="alert dark alert-alt alert-icon alert-{{ Session::get('message.0')  }} alert-dismissible" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+    @if(Session::get('message.0') == "success")
+        <i class="icon md-check" aria-hidden="true"></i>
+    @elseif(Session::get('message.0') == "warning")
+        <i class="icon md-alert-circle-o" aria-hidden="true"></i>
+    @elseif(Session::get('message.0') == "danger")
+        <i class="icon md-close" aria-hidden="true"></i>
+    @endif
+    {!! Session::get('message.1')  !!}
 </div>
