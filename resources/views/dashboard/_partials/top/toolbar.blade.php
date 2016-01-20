d9951feb26b49add7d78b06a6013e9092312d2cb
Mohamed Kawsara <mkwsra@gmail.com>
2016-01-20 09:19:39 +0200

    Adjust toolbar at top section of Dashboard

--- resources/views/dashboard/_partials/top/toolbar.blade.php
+++ resources/views/dashboard/_partials/top/toolbar.blade.php
@@ -10,7 +10,7 @@
             <i class="icon md-more" aria-hidden="true"></i>
         </button>
         <div class="navbar-brand navbar-brand-center site-gridmenu-toggle" data-toggle="gridmenu">
-            <img class="navbar-brand-logo" src="/assets/images/logo.png" title="Remark">
+            <img class="navbar-brand-logo" src="/assets/img/logo.png" title="Remark">
             <span class="navbar-brand-text"> {{ trans('strings.app_name') }}</span>
         </div>
 
@@ -20,19 +20,19 @@
         <div class="collapse navbar-collapse navbar-collapse-toolbar" id="site-navbar-collapse">
             <!-- Navbar Toolbar -->
             <ul class="nav navbar-toolbar">
-                <li class="hidden-float" id="toggleMenubar">
-                    <a data-toggle="menubar" href="#" role="button">
-                        <i class="icon hamburger hamburger-arrow-left">
-                            <span class="sr-only">Toggle menubar</span>
-                            <span class="hamburger-bar"></span>
-                        </i>
-                    </a>
-                </li>
-                <li class="hidden-xs" id="toggleFullscreen">
-                    <a class="icon icon-fullscreen" data-toggle="fullscreen" href="#" role="button">
-                        <span class="sr-only">Toggle fullscreen</span>
-                    </a>
-                </li>
+                {{--<li class="hidden-float" id="toggleMenubar">--}}
+                    {{--<a data-toggle="menubar" href="#" role="button">--}}
+                        {{--<i class="icon hamburger hamburger-arrow-left">--}}
+                            {{--<span class="sr-only">Toggle menubar</span>--}}
+                            {{--<span class="hamburger-bar"></span>--}}
+                        {{--</i>--}}
+                    {{--</a>--}}
+                {{--</li>--}}
+                {{--<li class="hidden-xs" id="toggleFullscreen">--}}
+                    {{--<a class="icon icon-fullscreen" data-toggle="fullscreen" href="#" role="button">--}}
+                        {{--<span class="sr-only">Toggle fullscreen</span>--}}
+                    {{--</a>--}}
+                {{--</li>--}}
 
                 {{--@include('dashboard._partials.top.mega_menu')--}}
             </ul>
