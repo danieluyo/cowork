d095af9bdafc0cf673ab0fb7d626f4fdbc338e78
Mohamed Kawsara <mkwsra@gmail.com>
2016-03-20 09:10:37 +0200

    Tweak admins_venues table to cascade down and delete on venue deletion

--- database/migrations/2016_01_07_091841_create_admin_venues_table.php
+++ database/migrations/2016_01_07_091841_create_admin_venues_table.php
@@ -1,35 +1,32 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
-class CreateAdminVenuesTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('admin_venues', function (Blueprint $table) {
-            $table->increments('id');
-            $table->unsignedInteger('admin_id')->index();
-            $table->unsignedInteger('venue_id')->index();
-            $table->timestamps();
+class CreateAdminVenuesTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create( 'admin_venues', function( Blueprint $table ) {
+			$table->increments( 'id' );
+			$table->unsignedInteger( 'admin_id' )->index();
+			$table->unsignedInteger( 'venue_id' )->index();
+			$table->timestamps();
 
-            $table->foreign('admin_id')->references('id')->on('users');
-            $table->foreign('venue_id')->references('id')->on('venues');
-        });
-    }
+			$table->foreign( 'admin_id' )->references( 'id' )->on( 'users' );
+			$table->foreign( 'venue_id' )->references( 'id' )->on( 'venues' )->onDelete( 'cascade' );
+		} );
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::drop('admin_venues');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::drop( 'admin_venues' );
+	}
 }
