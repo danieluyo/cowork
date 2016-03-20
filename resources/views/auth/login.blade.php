b9abcb71edf41537aa0040c937101c070dfbd4a7
Mohamed Kawsara <mkwsra@gmail.com>
2016-03-20 06:55:21 +0200

    add alt to logo tag

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
 <div class="panel">
     <div class="panel-body">
         <div class="brand">
-            <img class="brand-img" src="/assets/img/cowork_logo_200x80.png" alt="...">
+            <img class="brand-img" src="/assets/img/cowork_logo_200x80.png" alt="{{ trans('strings.app_name') }} logo">
             {{--<h2 class="brand-text font-size-18">{{ trans('strings.app_name') }}</h2>--}}
         </div>
         <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
